163bd9bd7053e20053e81b7bca5f99d534f25eec
S3verus <root@localhost.localdomain>
2018-12-08 21:00:32 +0330

    base of site

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>safe mode security</title>
+    <style type="text/css">
+
+    </style>
+  </head>
+  <body>
+      <div class="main">
+        <div class="header">
+            
+        </div>
+        <div class="center">
+            <div class="aside">
+
+            </div>
+            <div class="article">
+
+            </div>
+        </div>
+        <div class="footer">
+
+        </div>
+      </div>
+  </body>
+</html>
